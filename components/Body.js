c42e82e28b8cad6de13f0e821727739fb150a8c4
Deva Teja <devateja@Devas-MacBook-Pro.local>
2024-09-19 23:08:01 +0530

    episode-6

--- components/Body.js
+++ components/Body.js
@@ -1,36 +1,91 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import {resObj} from "../utils/mockData";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+// import {resObj} from "../utils/mockData";
 
 const Body = () => {
 
-    const [listOfRestaurant, setListOfRestaurant] = useState(resObj);
+    // const [listOfRestaurant, setListOfRestaurant] = useState(resObj);
+    const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+    const [filteredRestaurant, setfilteredRestaurant] = useState([]);
+
+    const [searchText, setsearchText] = useState("");
+
+    // Whenever the state variable update, react triggers a reconcilation cycle (re-renders the component)
+    console.log("body rendered");
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.2958104&lng=76.6393805&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+        const jsonData = await data.json();
+
+        // console.log(jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+
+        // optional chaining
+        setListOfRestaurant(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setfilteredRestaurant(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+
+    // Conditional rendering - rendering based on the codition
+    // if(listOfRestaurant.length === 0){
+    //     return <Shimmer />;
+    // }
     
-    return (
+    // Ternary operator for conditional rendering
+    return listOfRestaurant.length === 0 ? <Shimmer />: (
         <div className="body">
-            <button className="filter-btn" onClick={() => 
-                {
-                    const restaurantsData = listOfRestaurant.filter(
-                        (res) => res.info.avgRating > 4
-                    );
-                    //console.log(listOfRestaurant)
-                    setListOfRestaurant(restaurantsData);
+            <div className="filter">
+                <div className="search-container">
+                    <input className="search-input" onChange={
+                        (e) => {
+                            setsearchText(e.target.value);
+
+                        }
+                    } value={searchText}/>
+                    <button className="search-btn" onClick={
+                        () => {
+                            // Filter the restaurant cards and update the UI
+                            // searchText
+                            console.log(searchText);
+                            
+                            const searchedData = listOfRestaurant.filter(
+                                (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                            );
+
+                            setfilteredRestaurant(searchedData);
+                        }
+                    }>Search</button>
+                </div>
+           
+                <button className="filter-btn" onClick={() => 
+                    {
+                        const restaurantsData = listOfRestaurant.filter(
+                            (res) => res.info.avgRating > 4
+                        );
+                        //console.log(listOfRestaurant)
+                        setListOfRestaurant(restaurantsData);
+                    }
+                }>Top Rated Restaurant</button>
+                <button className="lessTimeDeliveryRes-btn"
+                onClick={
+                    () => {
+                        const lessTimeDeliveryRestaurant = listOfRestaurant.filter(
+                            (res) => res.info.sla.deliveryTime < 20
+                        )
+                        setListOfRestaurant(lessTimeDeliveryRestaurant)
+                    }
+                    
                 }
-            }>Top Rated Restaurant</button>
-            <button className="lessTimeDeliveryRes-btn"
-            onClick={
-                () => {
-                    const lessTimeDeliveryRestaurant = listOfRestaurant.filter(
-                        (res) => res.info.sla.deliveryTime < 20
-                    )
-                    setListOfRestaurant(lessTimeDeliveryRestaurant)
-                }
-                
-            }
-            >Less Time Delivery Restaurant</button>
+                >Less Time Delivery Restaurant</button>
+             </div>
             <div className="res-container">
                 {
-                    listOfRestaurant.map((restaurant) => (
+                    filteredRestaurant.map((restaurant) => (
                         <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                     ))
                 }
--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,10 @@
 import { APP_LOGO } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+
+    let [btnName, setbtnName] = useState("Login");
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -12,6 +16,11 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <li><button className="log-btn" onClick={
+                        () => {
+                            (btnName === "Login") ? setbtnName("Logout") : setbtnName("Login");
+                        }
+                    }>{btnName}</button></li>
                 </ul>
             </div>
         </div>
